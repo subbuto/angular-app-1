aa37419480bd7b618f214387b6cf89f94875414b
subbuto <112005384+subbuto@users.noreply.github.com>
2022-08-29 16:32:27 +0530

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -33,7 +33,7 @@ module.exports = function (config) {
       ]
     },
     reporters: ['progress', 'kjhtml'],
-    port: http://54.202.6.181,
+    port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
